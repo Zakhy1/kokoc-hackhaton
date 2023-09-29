9a4c1a35acb5e497141ce5b9cba923403349cc4c
zakhy <lirchzakhar@gmail.com>
2023-09-29 15:14:56 +0700

    fix org and user model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from organisations.models import Organisation
+
 
 class User(AbstractUser):
     is_boss = models.BooleanField(default=False)
     profile_image = models.ImageField(upload_to="profiles/%Y/%m/%d/")
+    company = models.ForeignKey(Organisation,
+                                on_delete=models.CASCADE,
+                                related_name="organisation",
+                                null=True, blank=True)
     # events = models.ManyToManyField
