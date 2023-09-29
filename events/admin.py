8c1d4743f3e911b03a96dc7b0e2fa8a42920cab6
zakhy <lirchzakhar@gmail.com>
2023-09-29 17:04:51 +0700

    add activity models and register

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from events.models import Activity, Period, Event, ActivityProof
+
+
+@admin.register(Activity)
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ["title"]
+    search_fields = ["title"]
+
+
+@admin.register(ActivityProof)
+class ActivityProofAdmin(admin.ModelAdmin):
+    list_display = ["user", "activity"]
+    list_filter = ["user", "activity"]
+    search_fields = ["user"]
+
+
+@admin.register(Period)
+class PeriodAdmin(admin.ModelAdmin):
+    list_display = ["start", "end"]
+    list_filter = ["start", "end"]
+    ordering = ["start"]
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "price", "activity", "period"]
+    list_filter = ["price", "period"]
+    search_fields = ["title"]
--- events/models.py
+++ events/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Activity(models.Model):
+    title = models.CharField(max_length=128)
+
+
+class Period(models.Model):
+    start = models.DateField()
+    end = models.DateField()
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(default=990)
+    activity = models.ForeignKey(Activity,
+                                 on_delete=models.CASCADE,
+                                 related_name="activity")
+    period = models.ForeignKey(Period,
+                               on_delete=models.CASCADE,
+                               related_name="period")
+
+
+class ActivityProof(models.Model):
+    user = models.ForeignKey("users.User",
+                             on_delete=models.CASCADE,
+                             related_name="users")
+    activity = models.ForeignKey("Activity",
+                                 on_delete=models.CASCADE,
+                                 related_name="activities")
+    screenshot = models.ImageField(upload_to="proofs/%Y/%m/%d/")
