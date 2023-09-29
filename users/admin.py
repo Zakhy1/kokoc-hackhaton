9ac3cd17cdfc5eab6d215d2f0be0e712dfda2f4b
zakhy <lirchzakhar@gmail.com>
2023-09-29 14:11:51 +0700

    extend user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "is_boss"]
+    search_fields = ["username", "email"]
+    ordering = ["id"]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_boss = models.BooleanField(default=False)
+    profile_image = models.ImageField(upload_to="profiles/%Y/%m/%d/")
+    # events = models.ManyToManyField
